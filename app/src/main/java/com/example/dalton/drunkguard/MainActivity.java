d311725bd084789216e3992d9cea67f22427ee31
Dalton Dabney <Dalton Dabney>
2016-09-12 11:35:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/dalton/drunkguard/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.dalton.drunkguard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button getTime;
+    private TimePicker timepicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void pickTime(){
+        getTime =  (Button) findViewById(R.id.button);
+        timepicker = (TimePicker) findViewById(R.id.timePicker);
+
+        getTime.setOnClickListener(new View.OnClickListener(){
+           public void onCLick(View v){
+
+           }
+        }
+        );
+    }
+}
